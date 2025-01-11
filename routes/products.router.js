59192149594f094e6fb65de7af5354d2b2ca2632
dritodev <pedroalejandrorosaperez@gmail.com>
2025-01-11 19:36:18 -0400

    creando status code para las petiones de productos

--- routes/products.router.js
+++ routes/products.router.js
@@ -30,18 +30,22 @@ router.get('/filter', (req, res) => {
 router.get('/:id', (req, res) => {
 
   const { id } = req.params;
-
-  res.json({
-    id,
-    name: "Producto 2",
-    price: 1300
-  });
-
+  if(id === '999'){
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  } else {
+      res.status(200).json({
+        id,
+        name: "Producto 2",
+        price: 2000,
+      });
+  }
 })
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
@@ -50,7 +54,7 @@ router.post('/', (req, res) => {
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
+  res.status(200).json({
     message: 'update',
     data: body,
     id,
@@ -59,7 +63,7 @@ router.patch('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
+  res.status(200).json({
     message: 'deleted',
     id,
   })
