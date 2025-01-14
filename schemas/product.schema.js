16bb223a53ec20caefd5d5a339d31d47cde355ee
dritodev <pedroalejandrorosaperez@gmail.com>
2025-01-14 00:00:17 -0400

    fix: cambiar tipo de validación de imagen de URL a URI en el esquema de producto

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const id = joi.string().uuid();
 const name = joi.string().min(3).max(15);
 const price = joi.number().integer().min(10)
-const image = joi.string().url();
+const image = joi.string().uri();
 
 const createProductSchema = joi.object({
   name: name.required(),
