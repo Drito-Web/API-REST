a1f793fcaa74198902b067a351aba570a0682e63
dritodev <pedroalejandrorosaperez@gmail.com>
2025-01-13 22:28:58 -0400

    modificando el límite de generación de productos en el método generate

--- services/products.service.js
+++ services/products.service.js
@@ -8,8 +8,8 @@ class ProductsService {
     this.generate();
   }
 
-  async generate() {
-    const limit = 100;
+  generate() {
+    const limit = 10;
     for (let index = 0; index < limit; index++) {
       this.products.push({
         id: faker.datatype.uuid(),
