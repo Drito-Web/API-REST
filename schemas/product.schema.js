3dd591d6b472f4f47d50567d8cec054f05870ec2
dritodev <pedroalejandrorosaperez@gmail.com>
2025-01-13 22:49:38 -0400

    feat: agregar validación de imagen en los esquemas de producto

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -1,26 +1,21 @@
 const joi = require('joi');
 
 const id = joi.string().uuid();
-const name = joi
-  .string()
-  .alphanum()
-  .min(3)
-  .max(15);
-
-const price = joi
-  .number()
-  .integer()
-  .min(10)
+const name = joi.string().min(3).max(15);
+const price = joi.number().integer().min(10)
+const image = joi.string().url();
 
 const createProductSchema = joi.object({
   name: name.required(),
   price: price.required(),
+  image: image.required(),
 
 });
 
 const updateProductSchema = joi.object({
   name: name,
   price: price,
+  image: image,
 });
 
 const getProductSchema = joi.object({
@@ -29,4 +24,3 @@ const getProductSchema = joi.object({
 });
 
 module.exports = { createProductSchema, updateProductSchema, getProductSchema };
-
