3791dfbe22b77cb022975e4fd242de59abdadb38
dritodev <pedroalejandrorosaperez@gmail.com>
2025-01-14 21:16:10 -0400

    fix: agregar soporte para CORS en la configuración del servidor

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 
 const { loadError, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());//para recibir en json
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("server Express funcionando")
